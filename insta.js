ddb925bc47c19749ca1da06cf200e976221d9255
Dev_Saiyam <70647163+saiyamdubey@users.noreply.github.com>
2024-01-23 23:54:08 +0530

    Add files via upload

--- insta.js
+++ insta.js
@@ -1,22 +1,68 @@
-const axios = require('axios');
-const fs = require('fs').promises;
+// const puppeteer = require('puppeteer');
 
-const imageUrl = 'https://www.instagram.com/p/CtBG7E9yH9H';
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
 
-async function downloadImage() {
-  try {
-    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+//   // Enable the network domain to capture network requests
+//   await page.goto('https://www.instagram.com/p/CtBG7E9yH9H');
+//   await page.setRequestInterception(true);
 
-    // Extract file extension from the URL
-    const fileExtension = Date.now();
+//   page.on('request', (request) => {
+//     // Capture only image requests
+//     if (request.resourceType() === 'image') {
+//       console.log('Image Request URL:', request.url());
+//       console.log('Image Request Headers:', request.headers());
+//     }
 
-    // Save the image to a file
-    await fs.writeFile(`downloaded_image.${fileExtension}`, response.data);
+//     request.continue();
+//   });
 
-    console.log('Image downloaded successfully.');
-  } catch (error) {
-    console.error('Error:', error.message);
+//   // Wait for a few seconds to capture network requests
+//   await page.waitForTimeout(5000);
+
+//   await browser.close();
+// })();
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://www.instagram.com/p/CtBG7E9yH9H");
+  await page.setRequestInterception(true);
+
+
+  page.on("request", (request) => {
+    // Capture only image requests
+    if (request.resourceType() === "image") {
+      // Check if the image request URL contains the Instagram CDN domain
+      if (request.url()) {
+        console.log("Main Image Request URL:", request.url());
+        console.log("Main Image Request Headers:", request.headers());
+      }
+    }
+
+    request.continue();
+  });
+
+  // Wait for a few seconds to capture network requests
+  await page.waitForTimeout(5000);
+
+  // Continue processing the main image request if found
+  if (mainImageRequest) {
+    const mainImageResponse = await mainImageRequest.response();
+    const mainImageUrl = mainImageRequest.url();
+    const mainImageHeaders = mainImageResponse.headers();
+
+    console.log("Main Image URL:", mainImageUrl);
+    console.log("Main Image Headers:", mainImageHeaders);
+  } else {
+    console.log("Main image not found.");
   }
-}
 
-downloadImage();
+  await browser.close();
+})();
